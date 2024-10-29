dcc38f78de3eb2333ab1a31cd82189999c6c92fc
MuxN4 <stev3npy@gmail.com>
2024-10-29 09:34:06 +0300

    Introduced Selection struct to manage a collection of Node instances

---
+++ internal/dom/selection.go
@@ -0,0 +1,45 @@
+package dom
+
+// Selection represents a collection of nodes
+type Selection struct {
+	nodes []*Node
+	doc   *Document
+}
+
+// NewSelection creates a new Selection instance
+func NewSelection(nodes []*Node, doc *Document) *Selection {
+	return &Selection{
+		nodes: nodes,
+		doc:   doc,
+	}
+}
+
+// First returns the first element in the selection or an empty Selection if none exists.
+func (s *Selection) First() *Selection {
+	if len(s.nodes) == 0 {
+		return NewSelection(nil, s.doc)
+	}
+	return NewSelection([]*Node{s.nodes[0]}, s.doc)
+}
+
+func (s *Selection) Last() *Selection {
+	if len(s.nodes) == 0 {
+		return NewSelection(nil, s.doc)
+	}
+	return NewSelection([]*Node{s.nodes[len(s.nodes)-1]}, s.doc)
+}
+
+// Eq returns the element at the specified index or an empty Selection if out of bounds
+func (s *Selection) Eq(index int) *Selection {
+	if index < 0 || index >= len(s.nodes) {
+		return NewSelection(nil, s.doc)
+	}
+	return NewSelection([]*Node{s.nodes[index]}, s.doc)
+}
+
+// Each iterates over the selection and executes the callback for each node
+func (s *Selection) Each(callback func(int, *Node)) {
+	for i, node := range s.nodes {
+		callback(i, node)
+	}
+}
