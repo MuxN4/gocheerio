5141cbd4224a3e23793716f68dbb03495d06908a
MuxN4 <stev3npy@gmail.com>
2024-10-30 15:27:00 +0300

    Add tokenizer tests for CSS selector parsing

---
+++ internal/selector/tokenizer_test.go
@@ -0,0 +1,49 @@
+package selector
+
+import "testing"
+
+func TestTokenizer(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected []Token
+	}{
+		{
+			input: "div.class#id",
+			expected: []Token{
+				{Type: TokenTypeTag, Value: "div"},
+				{Type: TokenTypeClass, Value: "class"},
+				{Type: TokenTypeID, Value: "id"},
+			},
+		},
+		{
+			input: "div[data-test='value']",
+			expected: []Token{
+				{Type: TokenTypeTag, Value: "div"},
+				{Type: TokenTypeAttribute, Value: "[data-test='value']"},
+			},
+		},
+		{
+			input: "div > p",
+			expected: []Token{
+				{Type: TokenTypeTag, Value: "div"},
+				{Type: TokenTypeCombinator, Value: ">"},
+				{Type: TokenTypeTag, Value: "p"},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.input, func(t *testing.T) {
+			tokenizer := NewTokenizer(tc.input)
+			for _, expected := range tc.expected {
+				token := tokenizer.nextToken()
+				if token == nil {
+					t.Fatalf("Expected token %v, got nil", expected)
+				}
+				if token.Type != expected.Type || token.Value != expected.Value {
+					t.Errorf("Expected token %v, got %v", expected, token)
+				}
+			}
+		})
+	}
+}
